736e3195530778bbfc1440e9123513469eda4e41
Gunnar <gunnaryonker@gmail.com>
2023-07-09 17:42:08 -0500

    File update

--- Project 1/animenewsnetwork.py
+++ Project 1/animenewsnetwork.py
@@ -2,10 +2,10 @@ import requests
 from bs4 import BeautifulSoup
 import csv
 
-# List for stored data to export to csv
+# Create a list for all of the data to export later to csv
 anime_data = []
 
-# Send a get request to Anime News Network
+# Send a get request
 url = "https://www.animenewsnetwork.com/encyclopedia/ratings-anime.php?top50=popular&n=1000"
 req = requests.get(url)
 
@@ -16,11 +16,12 @@ soup = BeautifulSoup(req.content, "html.parser")
 with open("html_content_tableB.txt", "a", encoding="utf-8") as html_file:
         html_file.write(soup.prettify())
 
-# Find data such as rankid, title, rating, votes on initial page
+# Find the rankid, title, rating, and vote data
 anime_table = soup.find("table", class_="encyc-ratings")
 anime_entries = anime_table.find_all("tr", bgcolor="#EEEEEE")
 
-for entry in anime_entries:
+# Counter to keep track of progress since this is a long process
+for i, entry in enumerate(anime_entries):
     rank = entry.find("td", class_="l")
     title = entry.find("td", class_="t").find("a")
     rating = entry.find("td", class_="r").get_text().strip()
@@ -28,16 +29,19 @@ for entry in anime_entries:
 
     rank_text = rank.get_text().strip() if rank is not None else ""
     title_text = title.get_text().strip() if title is not None else ""
-    show_type = title_text.split("(")[-1].strip(")") if "(" in title_text else ""
-    title_text = title_text.split("(")[0].strip()
+    show_type = ""
+    title_parts = title_text.split(" (")
+    if len(title_parts) > 1:
+        show_type = title_parts[-1].strip(")")
+    title_text = title_parts[0]
 
-    # Open nested url to find runtime and episode count of each anime show
+    # Open the nested url for more information on each title
     if title is not None:
         anime_url = "https://www.animenewsnetwork.com" + title["href"]
         anime_req = requests.get(anime_url)
         anime_soup = BeautifulSoup(anime_req.content, "html.parser")
 
-        # Find the runtime(minutes) and episode count
+        # Find the runtime(in minutes) and episodes values
         runtime = ""
         episodes = ""
         infotypes = anime_soup.find_all("div", class_="encyc-info-type")
@@ -46,15 +50,16 @@ for entry in anime_entries:
                 strong_text = infotype.strong.get_text().strip()
                 if strong_text == "Running time:":
                     runtime_text = infotype.span.get_text().strip()
-                    runtime = runtime_text.split(" ")[0]  # Extract the number of minutes not the whole phrase
+                    runtime = runtime_text.split(" ")[0]
 
                 elif strong_text == "Number of episodes:":
                     episodes = infotype.span.get_text().strip()
-        
-        # Add each entry to item list for export
-        anime_data.append([str(rank_text) + 'b', title_text, show_type, rating, votes, runtime, episodes])
 
-# Name of csv file
+    anime_data.append([str(rank_text) + 'b', title_text, show_type, rating, votes, runtime, episodes])
+
+    print(f"Processed entry {i+1} of {len(anime_entries)}")
+
+# Name the csv file
 csv_file = "tableB.csv"
 
 # Write the data to the csv file
